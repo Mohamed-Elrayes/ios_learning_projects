b0ab5d5e967061c554b611daccbdf4f183c3c4a3
Mohamed Salah <52123133+Mohamed-Elrayes@users.noreply.github.com>
2022-11-30 10:51:39 +0200

    i am poor

---
+++ i am poor/i am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am poor
+//
+//  Created by mo on 07/05/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
