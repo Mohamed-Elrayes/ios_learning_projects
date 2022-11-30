a7c1669c80b3fd2b6a46b8c8954763216d18ba23
Mohamed Salah <52123133+Mohamed-Elrayes@users.noreply.github.com>
2022-11-30 10:38:49 +0200

    first commit

---
+++ i am rich/i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by mo on 06/05/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
